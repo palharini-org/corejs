31b606ee13dc179112e1fe0a2a5ff3073dbd1bdb
Palharini <Fabiano_Palharini@Dell.com>
2020-04-04 20:13:12 -0300

    fork

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 server.get('/test', (req, res) => {
-    return res.json({'message': 'Hello World.'});
+    return res.json({'message': 'Hello World!'});
 });
 
 server.listen('3000');
